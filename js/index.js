463f5b15e638f1399c7876ad137aefcb5d33c5c2
Oluwademilade Olagoke <ooolagok@uwaterloo.ca>
2015-07-18 17:11:02 -0400

    Changed colours and fonts

--- js/index.js
+++ js/index.js
@@ -9,34 +9,30 @@ var linkTopOffset = 100;
 
 window.addEventListener("load", function(){
   //NAV BAR
-  if($(window).scrollTop() <= navbarTransparencyHeight){
-    $("#nav_bar").css("background-color","rgba(47, 79, 79, 0.7)");
-    navbarTransparency = true;
+  if ($(window).scrollTop() <= navbarTransparencyHeight){
+    $("#nav_bar").css("background-color", "rgba(47, 79, 79, 0)");
   }
-  $("#home_link").click(function() {
-    $("html,body").animate({
-        scrollTop: $("#home").offset().top - 80},
-        1100);
-  });
-  $("#about_link").click(function() {
-    $("html,body").animate({
-        scrollTop: $("#about").offset().top - 80},
-        1100);
-  });
-  $("#experience_link").click(function() {
-    $("html,body").animate({
-        scrollTop: $("#experience").offset().top - 80},
-        1100);
-  });
-  $("#hobbies_link").click(function() {
-    $("html,body").animate({
-        scrollTop: $("#hobbies").offset().top - 80},
-        1100);
-  });
-  $("#connect_link").click(function() {
-    $("html,body").animate({
-        scrollTop: $("#connect").offset().top - 80},
-        1100);
+  else{
+    $("#nav_bar").css("background-color", "rgba(47, 79, 79, 0.7)");
+  }
+
+  $("#nav_bar li").click(function(){
+    var id = this.id;
+    if(id == "home_link"){
+      $("html,body").animate({scrollTop: $("#home").offset().top - 80}, 1100);
+    }
+    else if(id == "about_link"){
+      $("html,body").animate({scrollTop: $("#about").offset().top - 80}, 1100);
+    }
+    else if(id == "experience_link"){
+      $("html,body").animate({scrollTop: $("#experience").offset().top - 80}, 1100);
+    }
+    else if(id == "hobbies_link"){
+      $("html,body").animate({scrollTop: $("#hobbies").offset().top - 80}, 1100);
+    }
+    else if(id == "connect_link"){
+      $("html,body").animate({scrollTop: $("#connect").offset().top - 80}, 1100);
+    }
   });
 
   $("#nav_bar li").hover(
@@ -51,51 +47,48 @@ window.addEventListener("load", function(){
   $(".exp_full").hide();
   //div drops
   $(".exp_img").click(function(){
+    //experience drops
+    var id = this.id;
+    if(id == "snake_image"){
+      $(".exp_full").not("#exp_full_snake").slideUp();
+      $("#exp_full_snake").slideToggle();
+    }
+    else if(id == "pWebsite_image"){
+      $(".exp_full").not("#exp_full_pWebsite").slideUp();
+      $("#exp_full_pWebsite").slideToggle();
+    }
+    else if(id == "URA_image"){
+      $(".exp_full").not("#exp_full_URA").slideUp();
+      $("#exp_full_URA").slideToggle();
+    }
+    else if(id == "mabels_image"){
+      $(".exp_full").not("#exp_full_mabels").slideUp();
+      $("#exp_full_mabels").slideToggle();
+    }
+    else if(id == "blackberry_image"){
+      $(".exp_full").not("#exp_full_blackberry").slideUp();
+      $("#exp_full_blackberry").slideToggle();
+    }
+    else if(id == "tictac_image"){
+      $(".exp_full").not("#exp_full_tictac").slideUp();
+      $("#exp_full_tictac").slideToggle();
+    }
+    else if(id == "fortune_image"){
+      $(".exp_full").not("#exp_full_fortune").slideUp();
+      $("#exp_full_fortune").slideToggle();
+    }
+
+    //plus to minus
     var htmlString = $(this).find(".exp_info").html();
     if(htmlString.indexOf("<h3>+</h3>") > -1){
       $(".exp_img").find(".exp_info").html("<h3>+</h3>");
       $(this).find(".exp_info").html("<h3>-</h3>");
-      $("html,body").animate({
-          scrollTop: $(this).offset().top - 10},
-          800);
+      $("html,body").animate({scrollTop: $(this).offset().top - 10}, 800);
     }
     else{
       $(this).find(".exp_info").html("<h3>+</h3>");
-      $("html,body").animate({
-          scrollTop: $(this).offset().top - 100},
-          700);
+      $("html,body").animate({scrollTop: $(this).offset().top - 100}, 700);
     }
-  });
-  //2015
-  $("#snake_image").click(function(){
-    $(".exp_full").not("#exp_full_snake").slideUp();
-    $("#exp_full_snake").slideToggle();
-  });
-  $("#pWebsite_image").click(function(){
-    $(".exp_full").not("#exp_full_pWebsite").slideUp();
-    $("#exp_full_pWebsite").slideToggle();
-  });
-  $("#URA_image").click(function(){
-    $(".exp_full").not("#exp_full_URA").slideUp();
-    $("#exp_full_URA").slideToggle();
-  });
-  //2014
-  $("#mabels_image").click(function(){
-    $(".exp_full").not("#exp_full_mabels").slideUp();
-    $("#exp_full_mabels").slideToggle();
-  });
-  $("#blackberry_image").click(function(){
-    $(".exp_full").not("#exp_full_blackberry").slideUp();
-    $("#exp_full_blackberry").slideToggle();
-  });
-  //2013
-  $("#tictac_image").click(function(){
-    $(".exp_full").not("#exp_full_tictac").slideUp();
-    $("#exp_full_tictac").slideToggle();
-  });
-  $("#fortune_image").click(function(){
-    $(".exp_full").not("#exp_full_fortune").slideUp();
-    $("#exp_full_fortune").slideToggle();
   });
 
   // HOBBIES
@@ -104,26 +97,25 @@ window.addEventListener("load", function(){
   // CONTACT
   $("#contact_form").hide();
   //clicks
-  $("#linkedin_connect").click(function(){
-    window.location.href = "https://ca.linkedin.com/pub/demi-olagoke/47/257/583";
-  });
-  $("#github_connect").click(function(){
-    window.location.href = "https://github.com/Demi-O";
-  });
-  $("#instagram_connect").click(function(){
-    window.location.href = "https://instagram.com/demiolagoke/";
-  });
-  $("#message_connect").click(function(){
-    $("#contact_form").slideToggle();
-    if($("#contact_form").is(":visible")) {
-      $("html,body").animate({
-          scrollTop: $("#message_connect").offset().top + 50},
-          400);
+  $(".connect_div").click(function(){
+    var id = this.id;
+    if(id == "linkedin_connect"){
+      window.location.href = "https://ca.linkedin.com/pub/demi-olagoke/47/257/583";
     }
-    else{
-      $("html,body").animate({
-          scrollTop: $("#message_connect").offset().top},
-          300);
+    else if(id == "github_connect"){
+      window.location.href = "https://github.com/Demi-O";
+    }
+    else if(id == "instagram_connect"){
+      window.location.href = "https://instagram.com/demiolagoke/";
+    }
+    else if(id == "message_connect"){
+      $("#contact_form").slideToggle();
+      if($("#contact_form").is(":visible")) {
+        $("html,body").animate({scrollTop: $("#message_connect").offset().top + 50}, 400);
+      }
+      else{
+        $("html,body").animate({scrollTop: $("#message_connect").offset().top}, 300);
+      }
     }
   });
   //animations
@@ -149,31 +141,23 @@ window.addEventListener("load", function(){
   formValidator.addValidation("message", "maxlen=400", "Max length for a message is 400 characters");
 });
 
-function isScrolledIntoView(element){
-  var docViewTop = $(window).scrollTop();
-  var docViewBottom = docViewTop + $(window).height();
-  var elementTop = $(element).offset().top;
-  var elementBottom = elementTop + $(element).height();
-  return ((elementBottom >= docViewTop) && (elementTop <= docViewBottom) && (elementBottom <= docViewBottom) && (elementTop >= docViewTop));
-}
-
 $(window).scroll(function(){
-  // if ($(window).scrollTop() <= navbarTransparencyHeight && navbarTransparency == false &&
-  // animatingNavbar == false) {
-  //   navbarTransparency = true;
-  //   animatingNavbar = true;
-  //   $("#nav_bar").stop().animate({backgroundColor:  "rgba(47, 79, 79, 0)"}, 400, function() {
-  //   	animatingNavbar = false;
-  //   });
-  // }
-  // else if ($(window).scrollTop() > navbarTransparencyHeight && navbarTransparency == true &&
-  // animatingNavbar == false){
-  //   navbarTransparency = false;
- // 	  animatingNavbar = true;
-  // 	$("#nav_bar").stop().animate({backgroundColor:  "rgba(47, 79, 79, 0.7)"}, 400, function() {
-  // 		animatingNavbar = false;
-  // 	});
- // 	}
+  if ($(window).scrollTop() <= navbarTransparencyHeight && navbarTransparency == false &&
+  animatingNavbar == false) {
+    navbarTransparency = true;
+    animatingNavbar = true;
+    $("#nav_bar").stop().animate({backgroundColor:  "rgba(47, 79, 79, 0)"}, 350, function() {
+    	animatingNavbar = false;
+    });
+  }
+  else if ($(window).scrollTop() > navbarTransparencyHeight && navbarTransparency == true &&
+  animatingNavbar == false){
+    navbarTransparency = false;
+ 	  animatingNavbar = true;
+  	$("#nav_bar").stop().animate({backgroundColor:  "rgba(47, 79, 79, 0.7)"}, 300, function() {
+  		animatingNavbar = false;
+  	});
+ 	}
 
   //Link border bottom loop
   //from bottom to top
